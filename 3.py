bb1ea0a6fe3bae5d2f1c632ee055963ee600503f
Benjamin Gilliland <benegilliland@gmail.com>
2024-05-12 19:31:05 +0100

    Improved solution to question 3

--- 3.py
+++ 3.py
@@ -1,26 +1,12 @@
 import math
 
-def is_prime(n):
-	if n == 1:
-		return False
-	if n % 2 == 0:
-		return True if n == 2 else False
-
+def largest_prime_factor(n):
 	root = math.floor(math.sqrt(n))
 
-	for i in range(3, root + 1):
+	for i in range(2, root + 1):
 		if n % i == 0:
-			return False
+			return largest_prime_factor(n // i)
 
-	return True
-
-def largest_prime_factor(n):
-	for i in range(2, n // 2 + 1):
-		if n % i == 0:
-			factor = n // i;
-			if is_prime(factor):
-				return factor
-
-	return -1
+	return n
 
 print(largest_prime_factor(600851475143))
